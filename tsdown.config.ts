20bac17b36f0b04173eb782ced8a360018ab153b
Ruggery Iury Corrêa <ruggeryiury@gmail.com>
2025-12-18 06:00:12 -0300

    Changed tsdown building config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,4 +4,6 @@ export default defineConfig({
   entry: ['src/index.ts'],
   banner: "// For more information, please visit set-default-options's GitHub repository:\n// https://github.com/ruggeryiury/set-default-options\n",
   fixedExtension: false,
+  minify: true,
+  unbundle: true,
 })
