594b380f3bd1cdd61c92f73f9d8d472b52c71408
Ruggery Iury Corrêa <ruggeryiury@gmail.com>
2026-01-22 14:32:07 -0800

    Dependencies update

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  banner: "// For more information, please visit set-default-options's GitHub repository:\n// https://github.com/ruggeryiury/set-default-options\n",
+  banner: "// For more information, please visit use-default-options's GitHub repository:\n// https://github.com/ruggeryiury/use-default-options\n",
+  platform: 'neutral',
   fixedExtension: false,
   minify: true,
-  unbundle: true,
 })
